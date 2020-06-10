bd7e4676d534b9151b684d95cfc46c9f9e58e1ce
jakubpartyka <j.partyka20@gmail.com>
2020-06-10 11:37:39 +0200

    login gui created

---
+++ task-6/src/Login.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Login {
+    private JPanel loginPanel;
+    private JTextField loginTextField;
+    private JPasswordField passwordPasswordField;
+    private JButton logInButton;
+    private JButton registerButton;
+    private JButton quitButton;
+
+    public JPanel getLoginPanel() {
+        return loginPanel;
+    }
+}
--- task-6/src/Main.java
+++ task-6/src/Main.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+
 public class Main {
+    private static JFrame frame;
     public static void main(String[] args) {
-        System.out.println("test");
+        //login
+        initFrame();
+    }
+
+    private static void initFrame(){
+        Login login = new Login();
+
+        frame = new JFrame("Login window");
+        frame.add(login.getLoginPanel());
+        frame.setSize(400,400);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
